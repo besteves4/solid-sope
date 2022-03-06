8b3445947b0c95f5cec3e4d3154286cf35542b12
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2022-03-06 20:19:09 +0100

    fix error

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,12 +34,8 @@ import Input from "./input.js";
 import personalData from "./personaldata.json";
 import purpose from "./purpose.json";
 
-const { session } = useSession();
-
 async function authenticatedFetch() {
-
-
-  // 3. Make authenticated requests by passing `fetch` to the solid-client functions.
+// 3. Make authenticated requests by passing `fetch` to the solid-client functions.
   // For example, the user must be someone with Read access to the specified URL.
   const myDataset = await getSolidDataset(
     "https://pod.inrupt.com/besteves/private/odrl_policies/", {
@@ -54,10 +50,8 @@ async function authenticatedFetch() {
   console.log(profile)
 }
 
-authenticatedFetch();
-
 export default function Home() {
-  // const { session } = useSession();
+  const { session } = useSession();
 
   // d3 tree diagram from https://bl.ocks.org/d3noob/8375092
   /* const treeData = [

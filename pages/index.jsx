e6d055c6df463dd876ffaf90f1dc84640768c3fb
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2022-03-06 02:35:27 +0100

    minor update

--- pages/index.jsx
+++ pages/index.jsx
@@ -293,7 +293,7 @@ export default function Home() {
         const filenameSave = `${podRoot}private/odrl_policies/${filename}`;
 
         const policiesContainer = getSolidDataset(`${podRoot}private/odrl_policies/`, { fetch: fetch });
-        console.log(getContainedResourceUrlAll(policiesContainer));
+        console.log(policiesContainer);
 
         try {
           // Save the SolidDataset

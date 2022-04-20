031804fe0f8a640ea94e03a70a6f08383e785561
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2022-04-20 14:52:20 +0200

    test tabs from @mui

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,6 +10,12 @@ import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
 import {
   createSolidDataset,
   createThing,
@@ -40,6 +46,12 @@ const policyTypes = [
 
 export default function Home() {
   const { session, sessionRequestInProgress } = useSession();
+
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   const [chosenPolicy, setChosenPolicy] = useState(policyTypes[0].value);
   const [policyFilename, setPolicyFilename] = useState("example-policy.ttl");
@@ -255,6 +267,20 @@ export default function Home() {
               &quot;Generate&quot; button.
             </div>
           </div>
+          <Box sx={{ width: '100%', typography: 'body1' }}>
+            <TabContext value={value}>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <TabList onChange={handleChange} aria-label="lab API tabs example">
+                  <Tab label="Item One" value="1" />
+                  <Tab label="Item Two" value="2" />
+                  <Tab label="Item Three" value="3" />
+                </TabList>
+              </Box>
+              <TabPanel value="1">Item One</TabPanel>
+              <TabPanel value="2">Item Two</TabPanel>
+              <TabPanel value="3">Item Three</TabPanel>
+            </TabContext>
+          </Box>
           <div className="row">
             <div className="left-col">
               <div className="container">

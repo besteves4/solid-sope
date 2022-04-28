004ba4817401d56d857a5a453eae0175355a9c26
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2022-04-28 22:07:37 +0200

    minor update

--- components/PersonalData.js
+++ components/PersonalData.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useSession } from "@inrupt/solid-ui-react";
-import "./styles.css";
 import * as d3 from "d3";
 
 function ForceGraph({ nodes }) {

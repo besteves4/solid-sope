d6fec3c48260e298805ebda286c64b8dc323b96b
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2022-04-28 22:05:16 +0200

    test d3 code

--- components/PersonalData.js
+++ components/PersonalData.js
@@ -1,35 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSession } from "@inrupt/solid-ui-react";
-import DropdownTreeSelect from "react-dropdown-tree-select";
+import "./styles.css";
+import * as d3 from "d3";
 
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
+function ForceGraph({ nodes }) {
+  const [animatedNodes, setAnimatedNodes] = useState([]);
 
-import {
-  createSolidDataset,
-  createThing,
-  setThing,
-  addUrl,
-  saveSolidDatasetAt,
-  getPodUrlAll,
-  getSolidDataset,
-  getContainedResourceUrlAll,
-} from "@inrupt/solid-client";
-import { RDF, ODRL } from "@inrupt/vocab-common-rdf";
-import { fetch } from "@inrupt/solid-client-authn-browser";
+  // re-create animation every time nodes change
+  useEffect(() => {
+    const simulation = d3.forceSimulation();
+
+    // update state on every frame
+    simulation.on("tick", () => {
+      setAnimatedNodes([...simulation.nodes()]);
+    });
+
+    // copy nodes into simulation
+    simulation.nodes([...nodes]);
+    // slow down with a small alpha
+    simulation.alpha(0.1).restart();
+
+    // stop simulation on unmount
+    return () => simulation.stop();
+  }, [nodes]);
+
+  return (
+    <g>
+      {animatedNodes.map((node) => (
+        <circle
+          cx={node.x}
+          cy={node.y}
+          r={node.r}
+          key={node.id}
+          stroke="black"
+          fill="transparent"
+        />
+      ))}
+    </g>
+  );
+}
 
 export function PersonalData() {
   const { session, sessionRequestInProgress } = useSession();
+
+  const nodes = d3.range(50).map((n) => {
+    return { id: n, r: 5 };
+  });
 
   if (sessionRequestInProgress) {
     return null;
   }
 
   return (
-    <div className="row">{session.info.webId}</div>
+    <div className="row">
+      <div className="App">
+        <h1>{session.info.webId}</h1>
+        <svg width="800" height="600">
+          <ForceGraph nodes={nodes} />
+        </svg>
+      </div>
+    </div>
     );
 }

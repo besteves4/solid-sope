8bef7fc3af348e1b7d8b5263714aea96bc9001cf
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2022-04-29 09:29:11 +0200

    Revert "Update PersonalData.js"
    This reverts commit ff91b260f4c008b11d72c141bf8677ac84822da7.

--- components/PersonalData.js
+++ components/PersonalData.js
@@ -1,27 +1,27 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { useSession } from "@inrupt/solid-ui-react";
 import * as d3 from "d3";
 
 function ForceGraph({ nodes }) {
   const [animatedNodes, setAnimatedNodes] = useState([]);
 
+  // re-create animation every time nodes change
   useEffect(() => {
-    const simulation = d3
-      .forceSimulation()
-      .force("x", d3.forceX(400))
-      .force("y", d3.forceY(300))
-      .force("charge", d3.forceManyBody().strength(charge))
-      .force("collision", d3.forceCollide(5));
+    const simulation = d3.forceSimulation().force("x", d3.forceX(400)).force("y", d3.forceY(300));
 
+    // update state on every frame
     simulation.on("tick", () => {
       setAnimatedNodes([...simulation.nodes()]);
     });
 
+    // copy nodes into simulation
     simulation.nodes([...nodes]);
+    // slow down with a small alpha
     simulation.alpha(0.1).restart();
 
+    // stop simulation on unmount
     return () => simulation.stop();
-  }, [nodes, charge]);
+  }, [nodes]);
 
   return (
     <g>
@@ -41,14 +41,10 @@ function ForceGraph({ nodes }) {
 
 export function PersonalData() {
   const { session, sessionRequestInProgress } = useSession();
-  const [charge, setCharge] = useState(-3);
-  const nodes = useMemo(
-    () =>
-      d3.range(50).map((n) => {
-        return { id: n, r: 5 };
-      }),
-    []
-  );
+
+  const nodes = d3.range(50).map((n) => {
+    return { id: n, r: 5 };
+  });
 
   if (sessionRequestInProgress) {
     return null;
@@ -58,16 +54,8 @@ export function PersonalData() {
     <div className="row">
       <div className="App">
         <h1>{session.info.webId}</h1>
-        <input
-          type="range"
-          min="-30"
-          max="30"
-          step="1"
-          value={charge}
-          onChange={(e) => setCharge(e.target.value)}
-        />
         <svg width="800" height="600">
-        <ForceGraph nodes={nodes} charge={charge} />
+          <ForceGraph nodes={nodes} />
         </svg>
       </div>
     </div>

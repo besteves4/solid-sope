ee625c3fbdd45452444969964502843e56182a52
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2023-07-07 00:14:01 +0200

    test new d3 force simulation code

--- components/ForceLayout.js
+++ components/ForceLayout.js
@@ -9,10 +9,11 @@ export default class ForceLayout extends React.PureComponent {
     width = canvas.width,
     height = canvas.height;
 
-    var simulation = d3.forceSimulation(data.nodes)
+/*     var simulation = d3.forceSimulation(data.nodes)
       .force("charge", d3.forceManyBody())
-      .force("link", d3.forceLink(data.links))
-      .force("center", d3.forceCenter());
+      .force("center", d3.forceCenter())
+      .force("link", d3.forceLink().links(data.links));
+      // .force("link", d3.forceLink(data.links));
       
     simulation
         .on("tick", ticked);
@@ -44,10 +45,88 @@ export default class ForceLayout extends React.PureComponent {
     function drawNode(d) {
       context.moveTo(d.x + 3, d.y);
       context.arc(d.x, d.y, 6, 0, 2 * Math.PI);
+    } */
+
+    var nodes = [
+      {name: 'A'},
+      {name: 'B'},
+      {name: 'C'},
+      {name: 'D'},
+      {name: 'E'},
+      {name: 'F'},
+      {name: 'G'},
+      {name: 'H'},
+    ]
+    
+    var links = [
+      {source: 0, target: 1},
+      {source: 0, target: 2},
+      {source: 0, target: 3},
+      {source: 1, target: 6},
+      {source: 3, target: 4},
+      {source: 3, target: 7},
+      {source: 4, target: 5},
+      {source: 4, target: 7}
+    ]
+
+    var simulation = d3.forceSimulation(nodes)
+      .force('charge', d3.forceManyBody().strength(-100))
+      .force('center', d3.forceCenter(width / 2, height / 2))
+      .force('link', d3.forceLink().links(links));
+    
+    simulation.on('tick', ticked);
+
+    function ticked() {
+      updateLinks()
+      updateNodes()
+    }
+
+    function updateLinks() {
+      var u = d3.select('.links')
+        .selectAll('line')
+        .data(links)
+        .join('line')
+        .attr('x1', function(d) {
+          return d.source.x
+        })
+        .attr('y1', function(d) {
+          return d.source.y
+        })
+        .attr('x2', function(d) {
+          return d.target.x
+        })
+        .attr('y2', function(d) {
+          return d.target.y
+        });
+    }
+
+    function updateNodes() {
+      u = d3.select('.nodes')
+        .selectAll('text')
+        .data(nodes)
+        .join('text')
+        .text(function(d) {
+          return d.name
+        })
+        .attr('x', function(d) {
+          return d.x
+        })
+        .attr('y', function(d) {
+          return d.y
+        })
+        .attr('dy', function(d) {
+          return 5
+        });
     }
   }
 
   render() {
-    return <canvas width="960" height="500"></canvas>;
+    // return <canvas width="960" height="500"></canvas>;
+    return (<div id="content">
+    <svg width="400" height="300">
+      <g class="links"></g>
+      <g class="nodes"></g>
+    </svg>
+  </div>);
   }
 }
